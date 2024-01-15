43cb13a428bda2309c99f61fae37ad20f48de49e
Robin de Boer <robindboer@gmail.com>
2024-01-15 11:46:09 +0100

    Disable closing cmp window with escape

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -20,7 +20,7 @@ cmp.setup {
     end,
     ["<CR>"] = cmp.mapping.confirm { select = true },
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<Esc>"] = cmp.mapping.close(),
+    -- ["<Esc>"] = cmp.mapping.close(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
   },
